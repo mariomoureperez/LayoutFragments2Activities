0136b91d832cb60f9ff44562ce0eaeb0ac3dd9b3
mario moure <mario moure>
2016-12-07 09:21:50 +0100

    Añadido intent getStringExtra y funcionando desde portrait

--- java/com/mario/layoutfragments/MainActivity2Fragment.java
+++ java/com/mario/layoutfragments/MainActivity2Fragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.mario.layoutfragments.R.id.textView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,10 +21,14 @@ public class MainActivity2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main2, container, false);
+        View layFra2=inflater.inflate(R.layout.fragment_main2, container, false);
+
+        TextView texto=(TextView) layFra2.findViewById(R.id.textView);
+        String str=getActivity().getIntent().getStringExtra("hola");
+        texto.setText(str);
+        return layFra2;
 
     }
-    
-    String str=getActivity().getIntent().getStringExtra("hola");
+
 
 }
